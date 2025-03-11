edb7ac978cc6d656e43c12a022ffe135f4f8a021
MasindiManaga <134053229+MasindiManaga@users.noreply.github.com>
2025-03-11 22:42:13 +0200

    Add files via upload

---
+++ Q3 assist.R
@@ -0,0 +1,26 @@
+
+library(readr)
+#system("quarto check") ~verify installation
+#install necessary packages:
+#install.packages("shiny")
+
+
+#print(names(subset_data))
+
+#
+#file.exists("subset_data.csv")
+
+#file.exists("C:/Users/user/Downloads/subset_data.csv")
+
+#setwd("C:/Users/user/Downloads")  
+#subset_data <- read_csv("subset_data.csv") 
+
+
+write_csv(subset_data, "C:/Users/user/Downloads/subset_data.csv")
+file.exists("C:/Users/user/Downloads/subset_data.csv")
+
+subset_data <- read_csv("subset_data.csv") 
+
+
+library(rsconnect)
+rsconnect::deployApp('C:/Users/user/Downloads/app')
